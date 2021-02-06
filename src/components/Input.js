a4024d3005f2b5176661a063b250af907dcac1de
salemeid <salem.eid@gmail.com>
2021-02-06 23:05:12 +0300

    fixed input output

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,10 +4,11 @@ import React from 'react';
 class InputNumber extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value:''};
+        this.state = {value:'', beta:''};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
 
     }
 
@@ -17,19 +18,31 @@ class InputNumber extends React.Component {
     }
 
     handleSubmit(event) {
+        
         console.log(this.state.value);
         event.preventDefault(0);
     }
 
+    handleClick() {
+        this.setState({beta: this.state.value})
+    }
+
+    
     render() {
-    return(       
+        
+    return(
+        <div>
         <form onSubmit= {this.handleSubmit}>
             <label>
             Name:
             <input type="text" value={this.state.value} onChange={this.handleChange} />
             </label>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" onClick={this.handleClick}/>
         </form>
+
+        <p>The name is {this.state.beta}</p>
+        </div>       
+        
     )}
     
 
