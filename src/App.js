3176dbf67041e74e69e3fd12d257c4bfcda48116
salemeid <salem.eid@gmail.com>
2021-02-07 23:35:34 +0300

    reconstruct code with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { useState } from 'react';
 import InputNumber from "./components/Input";
 
 
-function App() {
+const App = () => {
+
+  const [value, setValue] = useState('');
+  const [beta, setBeta] = useState('');
+
+
+
+
+const handleSubmit = (e) => {
+    console.log(value);
+    e.preventDefault(0);
+}
+
+
   return (
     <div>
       <h1>Hello World</h1>
-      <InputNumber />
+      <form onSubmit= {handleSubmit}>
+            <label>
+            Name:
+            {/* for updating value */}
+            <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} /> 
+            </label>
+            {/* for displaying value */}
+            <input type="submit" value="Submit" onClick={()=> setBeta(value)}/>
+        </form>
+      <InputNumber prn = {beta}/>
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,51 +1,10 @@
 import React from 'react';
 
 
-class InputNumber extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value:'', beta:''};
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-
-    }
-
- 
-    handleChange(event) {    
-        this.setState({value: event.target.value}); 
-    }
-
-    handleSubmit(event) {
-        
-        console.log(this.state.value);
-        event.preventDefault(0);
-    }
-
-    handleClick() {
-        this.setState({beta: this.state.value})
-    }
-
-    
-    render() {
-        
+const InputNumber = (props) => {       
     return(
         <div>
-        <form onSubmit= {this.handleSubmit}>
-            <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" onClick={this.handleClick}/>
-        </form>
-
-        <p>The name is {this.state.beta}</p>
-        </div>       
-        
+        <p>Your Payroll Number is  {props.prn}</p>
+        </div>             
     )}
-    
-
-}
-
 export default InputNumber;
